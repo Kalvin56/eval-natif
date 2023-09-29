d9100d1c577b0dfbf5ae1abec95db3c139fd7ad5
kalvin <kalvin.daniel56@laposte.net>
2023-09-29 10:28:17 +0200

    init

---
+++ exo2/exo2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Entrer du texte :");
+        var input = Console.ReadLine();
+        var inputMaj = ConvertToUpper(input);
+        Console.WriteLine(inputMaj);
+    }
+
+    public static string ConvertToUpper(string text)
+    {
+        return text.ToUpper();
+    }
+}
---
+++ exo4/exo4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Program
+{
+    static List<char> alphabet = new List<char>
+    {
+        'a','b','c','d','e','f','g','h','i','j','k','l','m',
+        'n','o','p','q','r','s','t','u','v','w','x','y','z'
+    };
+
+    public static void Main()
+    {
+        AlgoTest(MyNameIs);
+        Console.ReadLine();
+    }
+
+    public static string MyNameIs(string name)
+    {
+        var result = "";
+        foreach (char c in name)
+        {
+            char cmin = char.ToLower(c);
+            int index = alphabet.FindIndex(a => a == cmin);
+            result += index;
+        }
+        return result;
+        // myNameIs("Ahmed") should return  '071243'
+        // myNameIs("Dorlean") return 31417114013
+        // myNameIs("a") return  0
+        // myNameIs("Sandrine") return 180133178134
+
+
+    }
+
+    public static void AlgoTest(Func<string, string> func)
+    {
+        if (func("AB") == "01")
+        {
+            Console.WriteLine("Success");
+        }
+        else
+        {
+            Console.WriteLine("Fail");
+        }
+    }
+}
